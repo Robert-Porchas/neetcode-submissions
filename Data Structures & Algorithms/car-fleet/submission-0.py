3731580cc94dbedb1b0d40989f4f31f049971a8e
Robert Porchas <robertgeomaing@gmail.com>
2026-09-04 15:32:08 -0700

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pairs = [(position[i], speed[i]) for i in range(len(position))]
+        # putting them in pairs so that we can sort them without losing speed
+        carFleets = [] # will be the stack and output count
+        for pos, spe in sorted(pairs, reverse=True):
+            # iterate through the reverse sorted list to see which can combine
+            current = (target - pos) / spe # how long we have until reach end 
+            if len(carFleets) == 0:
+                carFleets.append(current)
+                continue
+            if current <= carFleets[-1]:
+                continue
+            else:
+                carFleets.append(current)       
+        return len(carFleets)
